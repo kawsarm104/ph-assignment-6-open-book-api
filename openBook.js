51bd62e287665eba29410245d83b4b6424e2af05
kawsarm104 <kawsarm104@gmail.com>
2021-09-01 22:43:00 +0600

    hide empty message and spinner

--- openBook.js
+++ openBook.js
@@ -1,1 +1,5 @@
 console.log("i am connected")
+    const searchField = document.getElementById('search-field')
+    document.getElementById('error-message').style.display = 'none';
+    document.getElementById('spinner').style.display = 'none';
+    const searchResult = document.getElementById('search-result');
