68b590514e13d74a906ede4c4888ff9fbeee7e70
kawsarm104 <kawsarm104@gmail.com>
2021-09-01 22:58:21 +0600

    added if condition for empty search

--- openBook.js
+++ openBook.js
@@ -3,3 +3,23 @@ console.log("i am connected")
     document.getElementById('error-message').style.display = 'none';
     document.getElementById('spinner').style.display = 'none';
     const searchResult = document.getElementById('search-result');
+
+const searchBook = () => {
+    const noResult = document.getElementById('no-result');
+    const searchText = searchField.value 
+    console.log(searchText)
+    searchField.value = ""
+    if (searchText == '') {
+        document.getElementById('error-message').style.display = 'none';
+        searchResult.textContent = '';
+        console.log("Empty String")
+        const p = document.createElement('p');
+        noResult.textContent = ''
+        p.style.color = 'red'
+        p.innerText = "Please write a book name"
+        noResult.appendChild(p)
+    }
+    else {
+        
+    }
+}
